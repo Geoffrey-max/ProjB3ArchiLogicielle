66c46c7255055acd56ca35ac27f85598c91bdd29
Geoffrey_max <geoffrey.clermont@ynov.com>
2019-11-19 14:52:36 +0100

    je suis une grosse ka

--- index.js
+++ index.js
@@ -1,43 +1,121 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const { pool } = require('./script/config')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { pool } = require("./script/config");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-const getBooks = (request, response) => {
-    pool.query('SELECT * FROM books', (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
+const getUser = (request, response) => {
+  pool.query("SELECT * FROM geneworld.users", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
 
-const addBook = (request, response) => {
-    const { author, title } = request.body
+const addUser = (request, response) => {
+  const { firstName, lastName, pwd, confirmePwd } = request.body;
+  if (pwd === confirmePwd) {
+    pool.query("INSERT INTO geneworld.users(firstName, lastName, password) VALUES ($1, $2, crypt($3, gen_salt('bf')));", [firstName, lastName, pwd], error => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).json({ status: "success", message: "User added." });
+    });
+  } else {
+    response.status(500).json({ status: "unsuccess", message: "User not added." });
+  }
+};
 
-    pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
-        if (error) {
-            throw error
-        }
-        response.status(201).json({ status: 'success', message: 'Book added.' })
-    })
-}
+const getObstacle = (request, response) => {
+  pool.query("SELECT * FROM geneworld.obstacles", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const addObstacle = (request, response) => {
+  const { traversable, effect, nom, type, positionX, positionY, width, height } = request.body;
+  pool.query("INSERT INTO geneworld.obstacles(traversable, effect, nom, type, positionX, positionY, width, height) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);", [traversable, effect, nom, type, positionX, positionY, width, height], error => {
+    if (error) {
+      throw error;
+    }
+    response.status(201).json({ status: "success", message: "Obstacle added." });
+  });
+};
+
+const getVague = (request, response) => {
+  pool.query("SELECT * FROM geneworld.vagues", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const addVague = (request, response) => {
+  const { nbr, zone_aggro, vitesse, temps_btw_apparission, autre } = request.body;
+  pool.query("INSERT INTO geneworld.vagues(nbr, zone_aggro, vitesse, temps_btw_apparission, autre) VALUES ($1, $2, $3, $4, $5);", [nbr, zone_aggro, vitesse, temps_btw_apparission, autre], error => {
+    if (error) {
+      throw error;
+    }
+    response.status(201).json({ status: "success", message: "Vague added." });
+  });
+};
+
+const getHistorique = (request, response) => {
+  pool.query("SELECT * FROM geneworld.historiques", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const addHistorique = (request, response) => {
+  const { score, datepassage, id_joueurs } = request.body;
+  pool.query("INSERT INTO geneworld.vagues(score, datepassage, id_joueurs) VALUES ($1, $2, $3);", [score, datepassage, id_joueurs], error => {
+    if (error) {
+      throw error;
+    }
+    response.status(201).json({ status: "success", message: "Historique added." });
+  });
+};
 
 app
-    .route('/books')
-    // GET endpoint
-    .get(getBooks)
-    // POST endpoint
-    .post(addBook)
+  .route("/users")
+  // GET endpoint
+  .get(getUser)
+  // POST endpoint
+  .post(addUser);
+
+
+
+app
+  .route("/obstacles")
+  .get(getObstacle)
+  .post(addObstacle);
+
+
+app
+  .route("/vagues")
+  .get(getVague)
+  .post(addVague);
+
+app
+  .route("/historiques")
+  .get(getHistorique)
+  .post(addHistorique);
 
 
 // Start server
 app.listen(process.env.PORT || 3002, () => {
-    console.log(`Server listening`,process.env.PORT || 3002)
-})
+  console.log(`Server listening`, process.env.PORT || 3002);
+});
